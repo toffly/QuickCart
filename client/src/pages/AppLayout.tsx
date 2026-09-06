b17aac4d68af976504ea78e0ae43f5d769c81102
toffly <toffly@outlook.com>
2026-09-06 12:48:52 +0700

    implement routing and layout structure with new pages for enhanced navigation

--- client/src/pages/AppLayout.tsx
+++ client/src/pages/AppLayout.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
-    <div>AppLayout</div>
-  )
-}
+    <>
+      <p>banner</p>
+      <p>navbar</p>
+      <main className="min-h-screen">
+        <Outlet />
+      </main>
+      <p>footer</p>
+      <p>cartsidebar</p>
+    </>
+  );
+};
 
-export default AppLayout
+export default AppLayout;

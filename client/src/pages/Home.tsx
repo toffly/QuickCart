cc8bb81d48b0a7b2ea51af2761ca702092bc3eb1
toffly <toffly@outlook.com>
2026-09-07 14:14:51 +0700

    add HomeCategories component to display product categories on Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Features from '../components/Home/Features'
 import Hero from '../components/Home/Hero'
+import HomeCategories from '../components/Home/HomeCategories'
 
 const Home = () => {
   return (
     <div className='max-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12'>
       <Hero/>
       <Features/>
+      <HomeCategories/>
     </div>
   )
 }

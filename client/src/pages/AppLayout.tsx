bdf7ebfa7066dcccbbd6c51bc2465c75d0526412
toffly <toffly@outlook.com>
2026-09-07 12:50:18 +0700

    add Banner component for promotional messaging and integrate into AppLayout

--- client/src/pages/AppLayout.tsx
+++ client/src/pages/AppLayout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Banner from "../components/Banner";
 
 const AppLayout = () => {
   return (
     <>
-      <p>banner</p>
+      <Banner />
       <p>navbar</p>
       <main className="min-h-screen">
         <Outlet />

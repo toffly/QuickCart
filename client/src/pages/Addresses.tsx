1105a3074ce7d6eb3edeb1906d751f5dcbbd6b48
toffly <toffly@outlook.com>
2026-09-09 13:47:10 +0700

    feat: implement Addresses page with address management functionality

--- client/src/pages/Addresses.tsx
+++ client/src/pages/Addresses.tsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import type { Address } from "../types";
+import { dummyAddressData } from "../assets/assets";
+import { PlusIcon } from "lucide-react";
 
 const Addresses = () => {
-  return (
-    <div>Addresses</div>
-  )
-}
+  const [Addresses, setAddresses] = useState<Address[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [showForm, setShowForm] = useState(false);
+  const [editingId, setEditingId] = useState<string | null>(null);
+  const [form, setForm] = useState({
+    label: "",
+    address: "",
+    city: "",
+    state: "",
+    zip: "",
+    isDefault: false,
+  });
 
-export default Addresses
+  const resetForm = () => {
+    setForm({
+      label: "",
+      address: "",
+      city: "",
+      state: "",
+      zip: "",
+      isDefault: false,
+    });
+    setShowForm(false);
+    setEditingId(null);
+  };
+
+  const handleSubmit = (e: React.SubmitEvent) => {
+    e.preventDefault();
+  };
+
+  const onEditHandler = (add: Address) => {
+    setForm({
+      label: add.label,
+      address: add.address,
+      city: add.city,
+      state: add.state,
+      zip: add.zip,
+      isDefault: add.isDefault,
+    });
+    setEditingId(add._id);
+    setShowForm(true);
+  };
+
+  useEffect(() => {
+    const fetchData = () => {
+      setAddresses(dummyAddressData);
+      setTimeout(() => setLoading(false), 1000);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-app-cream">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <div className="flex items-center justify-between mb-8">
+          <h1 className="text-2xl font-semibold text-app-green">
+            My Addresses
+          </h1>
+          <button
+            onClick={() => {
+              resetForm();
+              setShowForm(true);
+            }}
+            className="px-4 py-2 bg-app-green text-white text-sm font-semibold rounded-xl hover:bg-app-green-light transition-colors flex items-center gap-2"
+          >
+            <PlusIcon className="size-4" /> Add Address
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Addresses;

8f9bf275fe0984b16c5a657bb3b7a897d0cf8db1
toffly <toffly@outlook.com>
2026-09-11 15:05:56 +0700

    fix(admin): correct order of return statement in admin middleware

--- server/middleware/admin.ts
+++ server/middleware/admin.ts
@@ -15,8 +15,8 @@ const admin = async (req: Request, res: Response, next: NextFunction) => {
     const adminEmails = process.env.ADMIN_EMAILS ? process.env.ADMIN_EMAILS.split(",").map((e)=> e.trim().toLowerCase()) : []
 
     if(adminEmails.includes(user.email.toLowerCase())) {
-        if(req.user) return req.user.isAdmin = true
-        next()
+        if(req.user) req.user.isAdmin = true
+        return next()
     } else {
         res.status(403).json({message: "Admin access required"})
     }

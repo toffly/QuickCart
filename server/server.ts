b1c88db61caa36398ab43462213d0b6c8cfd9d1b
toffly <toffly@outlook.com>
2026-09-11 14:06:37 +0700

    feat(auth): implement user registration and login functionality with JWT and bcrypt

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -13,6 +14,13 @@ const port = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+app.use('/api/auth', authRouter)
+
+//Error Handler
+app.use((error: any,req: Request, res: Response, next: NextFunction)=> {
+    console.error()
+    res.status(500).json({message: error})
+})
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);

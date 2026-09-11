0fd1bffb1b650477b402a0984b5c615d5536ad81
toffly <toffly@outlook.com>
2026-09-11 15:21:59 +0700

    feat(upload): implement image upload route with Cloudinary integration

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
 import productRouter from "./routes/productRoutes.js";
+import uploadRouter from "./routes/uploadRoutes.js";
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.get('/', (req: Request, res: Response) => {
 });
 app.use('/api/auth', authRouter)
 app.use('/api/products', productRouter)
+app.use('/api/upload', uploadRouter)
 
 //Error Handler
 app.use((error: any,req: Request, res: Response, next: NextFunction)=> {

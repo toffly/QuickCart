d3909e82efc61eb4e717f658f992b18ecdc5584b
toffly <toffly@outlook.com>
2026-09-11 15:39:34 +0700

    feat(order): implement createOrder function for processing orders

---
+++ server/controller/orderController.ts
@@ -0,0 +1,83 @@
+// POST
+
+import { Request, Response } from "express";
+import { prisma } from "../config/prisma.js";
+
+// /api/orders
+export const createOrder = async (req: Request, res: Response) => {
+  const { items, shippingAddress, paymentMethod } = req.body;
+
+  if (!items || items.length === 0) {
+    return res.status(400).json({ message: "No order items" });
+  }
+
+  const productIds = items.map((i: any) => i.product);
+  const products = await prisma.product.findMany({
+    where: { id: { in: productIds } },
+  });
+  const productMap: Record<string, (typeof products)[0]> = {};
+
+  products.forEach((p: any) => (productMap[p.id] = p));
+
+  for (const item of items) {
+    const product = productMap[item.product];
+    if (!product || (product.stock ?? 0) < item.quantity) {
+      return res.status(404).json({ message: "Product out of stock" });
+    }
+  }
+
+  const orderItems = items.map((item: any) => {
+    const dbProduct = productMap[item.product];
+    if (!dbProduct) throw new Error(`Product ${item.product} not found`);
+    return {
+      product: dbProduct.id,
+      name: dbProduct.name,
+      image: dbProduct.image,
+      price: dbProduct.price,
+      quantity: item.quantity,
+      unit: dbProduct.unit,
+    };
+  });
+
+  const subtotal = orderItems.reduce(
+    (sum: number, item: any) => sum + item.price * item.quantity,
+    0,
+  );
+  const deliveryFee = subtotal > 20 ? 0 : 1.99;
+  const tax = Math.round(subtotal * 0.08 * 100) / 100;
+  const total = Math.round((subtotal + deliveryFee + tax) * 100) / 100;
+
+  const order = await prisma.order.create({
+    data: {
+      userId: req.user!.id,
+      items: orderItems,
+      shippingAddress,
+      paymentMethod,
+      subtotal,
+      deliveryFee,
+      tax,
+      total,
+      statusHistory: [
+        {
+          status: "Placed",
+          note: "Order placed successfully",
+          timestamp: new Date(),
+        },
+      ],
+    },
+  });
+
+  if (paymentMethod === "cart") {
+    //stripe payment link
+  }
+
+  res.json({ order });
+
+  //Decrease stock
+  for (const item of orderItems) {
+    await prisma.product.update({
+      where: { id: item.product },
+      data: { stock: { decrement: item.quantity } },
+    });
+  }
+};

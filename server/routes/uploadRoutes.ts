4e41d4195223a575f8d387750ce4256c8114984e
toffly <toffly@outlook.com>
2026-09-11 15:01:32 +0700

    fix(prisma): correct import path for PrismaClient feat(upload): add upload routes with authentication middleware

---
+++ server/routes/uploadRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import auth from "../middleware/auth.js"
+
+const router = express.Router()
+
+router.post('/', auth, async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }    
+})

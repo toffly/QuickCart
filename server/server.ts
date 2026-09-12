de5ef7c21b4adf326e4458015c4faa3546fb392b
toffly <toffly@outlook.com>
2026-09-12 14:26:54 +0700

    feat(order): add order routes for managing orders and their statuses

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import uploadRouter from "./routes/uploadRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api/auth', authRouter)
 app.use('/api/products', productRouter)
 app.use('/api/upload', uploadRouter)
+app.use('/api/orders', orderRouter)
 
 //Error Handler
 app.use((error: any,req: Request, res: Response, next: NextFunction)=> {

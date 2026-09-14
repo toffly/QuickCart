7189a95fe36f8155d841711080d116ddee6f207c
toffly <toffly@outlook.com>
2026-09-14 12:43:54 +0700

    feat(inngest): implement low stock alert and monthly offers email functionality

--- server/config/nodemailer.ts
+++ server/config/nodemailer.ts
@@ -2,7 +2,7 @@ import {createTransport} from 'nodemailer'
 
 // Create a transporter using SMTP
 const transporter = createTransport({
-  host: "smtp.example.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   auth: {
     user: process.env.SMTP_USER,
@@ -12,7 +12,7 @@ const transporter = createTransport({
 
 const sendEmail = async({ to, subject, body}: {to: string, subject: string, body: string}) => {
     const response = await transporter.sendMail({
-        from: "",
+        from: "process.env.SENDER_EMAIL",
         to,
         subject,
         html: body,

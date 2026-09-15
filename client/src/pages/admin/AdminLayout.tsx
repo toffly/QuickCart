1d71785bc98c2c95731c0509c8670d574232b06e
toffly <toffly@outlook.com>
2026-09-15 13:57:46 +0700

    feat(auth): enhance ProtectedRoute and AdminLayout with user authentication checks

--- client/src/pages/admin/AdminLayout.tsx
+++ client/src/pages/admin/AdminLayout.tsx
@@ -1,8 +1,10 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Navigate, NavLink, Outlet } from "react-router-dom";
 import { PlusIcon, PackageSearchIcon, ShoppingBagIcon, LogOutIcon, BarChart3Icon, ShieldIcon, Truck } from "lucide-react";
 import Navbar from "../../components/Navbar";
+import { useAuth } from "../../context/authContext";
 
 export default function AdminLayout() {
+    const {user} = useAuth()
 
     const AdminLinkData = [
         { to: "/admin", label: "Dashboard", icon: BarChart3Icon },
@@ -12,6 +14,10 @@ export default function AdminLayout() {
         { to: "/admin/delivery-partners", label: "Delivery Partners", icon: Truck },
         { to: "/", label: "Exit", icon: LogOutIcon },
     ]
+
+    if(!user?.isAdmin){
+        return <Navigate to={'/'} replace/>
+    }
 
     return (
         <div className="h-screen overflow-hidden">

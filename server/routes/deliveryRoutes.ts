db2f57c353eacbd21da0b039c6f6f4b49ce18e99
toffly <toffly@outlook.com>
2026-09-17 12:36:53 +0700

    fix(deliveryRoutes): simplify route definitions for better readability

--- server/routes/deliveryRoutes.ts
+++ server/routes/deliveryRoutes.ts
@@ -14,30 +14,18 @@ const deliveryRouter = express.Router();
 
 deliveryRouter.post("/login", loginPartner);
 deliveryRouter.get("/my-deliveries", deliveryAuth, getMyDeliveries);
-deliveryRouter.get(
-  "/my-deliveries/:id",
-  deliveryAuth,
-  getDeliveryDetail,
-);
+deliveryRouter.get("/my-deliveries/:id", deliveryAuth, getDeliveryDetail);
 deliveryRouter.put(
   "/my-deliveries/:id/complete",
   deliveryAuth,
   completeDelivery,
 );
-deliveryRouter.put(
-  "/my-deliveries/:id/cancel",
-  deliveryAuth,
-  cancelDelivery,
-);
+deliveryRouter.put("/my-deliveries/:id/cancel", deliveryAuth, cancelDelivery);
 deliveryRouter.put(
   "/my-deliveries/:id/status",
   deliveryAuth,
   updateDeliveryStatus,
 );
-deliveryRouter.put(
-  "/my-deliveries/:id/location",
-  deliveryAuth,
-  updateLocation,
-);
+deliveryRouter.put("/my-deliveries/:id/location", deliveryAuth, updateLocation);
 
 export default deliveryRouter;
